7750837eb1ae867b39b44d71d2af504d3ff4071b
DanieleGazzola <danielegazzola2000@gmail.com>
2022-11-29 15:34:53 +0100

    lettura input da file

--- Code/main.cpp
+++ Code/main.cpp
@@ -3,7 +3,46 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <array>
 
 int main(int argc, char** argv){
-    std::cout << "Hello" << std::endl;
+
+    if(argc != 2 )
+        return -1;
+
+    //input file
+    std::ifstream in_file;
+    in_file.open(argv[1]);
+    if(!in_file)
+        return -1;
+
+    //hyper-space dimension
+    std::size_t dimensions;
+    in_file >> dimensions;
+
+    std::vector<float> function;
+    std::vector<std::vector<float>> hyper_planes;
+    float in_data;
+
+    //function
+    for (int i = 0; i < dimensions + 1; ++i){
+        in_file >> in_data;
+        function.push_back(in_data);
+    }
+
+    //hyper-planes
+    while(!in_file.eof()){
+        std::vector<float> hyper_plane;
+        for(int i = 0; i < dimensions + 1; ++i){
+            in_file >> in_data;
+            hyper_plane.push_back(in_data);
+        }
+        hyper_planes.push_back(hyper_plane);
+    }
+
+
+    return 0;
+
 }
